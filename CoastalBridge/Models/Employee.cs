bb3772038bcfa96f2a7392e58d7dc4e4c830cdc0
Jeffrey Silva <jeffrey.silva@jeffworld.onmicrosoft.com>
2018-12-01 13:23:00 -0500

    Add IdentityUser and extending Auth to Employee

--- CoastalBridge/Models/Employee.cs
+++ CoastalBridge/Models/Employee.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.IdentityModel;
+using Microsoft.AspNetCore.Identity;
 
 namespace CoastalBridge.Models
 {
@@ -12,14 +14,12 @@ namespace CoastalBridge.Models
         Exit
     }
 
-    public class Employee
+    public class Employee : IdentityUser
     {
         public int EmployeeID { get; set; }
         public string FirstName { get; set; }
         public string MiddleName { get; set; }
         public string LastName { get; set; }
-
-        public string Email { get; set; }
 
         public string Title { get; set; }
 
